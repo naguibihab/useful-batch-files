2471816a5e4ff82664594014d5095f4c38d238bf
Chathuranga Abeyrathna <UABEYC1@LKSRIWL-BVNPP72.PEROOT.com>
2017-10-04 15:28:19 +0530

    myapp download script updated

---
+++ general/DownloadMyApps.bat
@@ -0,0 +1,19 @@
+@echo off
+SET app_list=app_list.txt
+SET CURRENTDIR="%cd%"
+SET app_path=%CURRENTDIR%/apps
+
+mkdir %app_path%
+
+for /f %%i in (%app_list%) do (
+    echo "Downloading %%i..."
+    cd %app_path%
+    curl %%i -LO > nul
+)
+
+cd %app_path%
+dir /b > zdownload_list.txt
+for /f %%i in (zdownload_list.txt) do (
+    echo "Installing %%i"
+    %app_path%\%%i /S
+)
