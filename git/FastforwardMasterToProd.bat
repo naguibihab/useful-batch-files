9184030a2f104b38fb98d656fb35963bc92a3f97
Nick Ihab <n.ihab@learnmeter.com>
2017-11-06 09:20:06 +1100

    Added a new batch file FastforwardMasterToProd & created a git folder to contain all git specific batch files

---
+++ git/FastforwardMasterToProd.bat
@@ -0,0 +1,22 @@
+REM ********************************************************************************
+REM								FastforwardMasterToProd.bat
+REM Fast forwards the branch `master` to the branch `production`.
+REM
+REM prerequisits: master must not be deviated from production
+REM ********************************************************************************
+
+::setup
+if "%isSetupCalled%" equ "" (
+call %BATCHLOCATION%/SetupEnv.bat
+)
+set _isPause=%isPause%
+
+::operations
+%frontend_directory%:
+cd %frontend_secondary_source_code%
+call git checkout master --force
+call git pull --force
+call git merge origin/production
+call git push
+
+if %_isPause% equ true pause
