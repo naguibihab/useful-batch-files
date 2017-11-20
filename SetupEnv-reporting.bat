a98e4d30d5b69357e89b0316062247266e3b589e
Nick Ihab <n.ihab@learnmeter.com>
2017-11-21 09:52:40 +1100

    Added a new environment & modified a few things

--- SetupEnv-reporting.bat
+++ SetupEnv-reporting.bat
@@ -1,5 +1,5 @@
 :: Teacher Project
-@echo off
+@echo on
 :: IMPORTANT PREREQUSITE: You must setup an environment variable by the name BATCHLOCATION pointing to the 
 :: directory that contains these batch files
 
@@ -11,18 +11,18 @@ SET isSetupCalled=true
 SET isPause=false
 
 :: The base directory where your source code is located
-SET frontend_directory=D
+SET frontend_directory=
 SET backend_directory=C
 
 :: The folders where your primary & secondary source code is located
 :: I like to have two source code folders, if you don't then just have them pointing to the same folder
-SET frontend_primary_source_code=\Dev\App
-SET frontend_secondary_source_code=\Dev\App2
-SET backend_primary_source_code=\xampp7\htdocs\php-teacher
-SET backend_secondary_source_code=\xampp7\htdocs\php-teacher2
+SET frontend_primary_source_code=
+SET frontend_secondary_source_code=
+SET backend_primary_source_code=\xampp7\htdocs\edquire-api
+SET backend_secondary_source_code=\xampp7\htdocs\edquire-api2
 
 :: Frameworks used. We use these as prefixes in some batch files
-SET frontend_framework=angular
+SET frontend_framework=
 SET backend_framework=slim
 
 :: Location of the downloadable url of the apps you use and where they get downloaded
@@ -46,7 +46,7 @@ SET base_url=http://10.0.2.65
 :::::::::::::::::::::::::::::::::::::::::: Angular :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: If you're using angular set these up
 :: The folder where you built code is copied
-SET build_folder=dist
+SET build_folder=
 
 
 :::::::::::::::::::::::::::::::::::::::::: AWS :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -56,14 +56,14 @@ SET build_folder=dist
 ::::::: S3 ::::::::
 :: dev_s3_bucket is considered the default bucket in all batch files
 :: Any sensetive data can be stored as an environment variable
-SET dev_s3_bucket=%ENV_dev_s3_bucket%
-SET stg_s3_bucket=%ENV_stg_s3_bucket%
-SET prod_s3_bucket=%ENV_prod_s3_bucket%
+SET dev_s3_bucket=
+SET stg_s3_bucket=
+SET prod_s3_bucket=
 
 ::::::: Cloud Front ::::::
-set dev_cf_distribution_id=%ENV_dev_cf_dist%
-set stg_cf_distribution_id=%ENV_stg_cf_dist%
-set prod_cf_distribution_id=%ENV_prod_cf_dist%
+set dev_cf_distribution_id=
+set stg_cf_distribution_id=
+set prod_cf_distribution_id=
 
 ::::::: RDS ::::::
 set rds_list=rds-instance-1 rds-instance-2
